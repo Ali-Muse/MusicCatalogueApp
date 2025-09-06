319d0eaf3dd7eb7d51bd19c380c3e1dbecd598a2
Ali-Mussa <alimuse526@gmail.com>
2025-09-06 20:03:33 +0300

    Update MusicCatalogueApp.java
    i made new cimmit

--- Advanced Object Oriented Programming Project/MusicCatalogueApp.java
+++ Advanced Object Oriented Programming Project/MusicCatalogueApp.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 public class MusicCatalogueApp {
 
     public static void main(String[] args) {
-        new MusicCatalogueApp();
+        new MusicCatalogueApp()
     }
 
 
